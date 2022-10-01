b14676a2714cc6d11c795bfc637fae66cd487289
mbos2 <mbosnjak2@outlook.com>
2022-10-01 17:44:33 +0200

    Added react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()],
+  integrations: [compress(), react()]
 });
